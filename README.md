5e012eef702d52642a03c345c31041e125d91439
Abner_G <grh_1990@126.com>
2018-01-05 17:54:48 +0800

    add

---
+++ README.md
@@ -0,0 +1,2 @@
+# RHProgressHUD
+RHProgressHUD 是一款简单的网络加载框
---
+++ RHProgressHUD/RHProgressHUD.h
@@ -0,0 +1,28 @@
+//
+//  RHProgressHUD.h
+//  RHKit
+//
+//  Created by 郭人豪 on 2017/9/8.
+//  Copyright © 2017年 Abner_G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RHProgressHUD : NSObject
+
+/**
+ 添加hud
+ 
+ @param targetView  要添加的view
+ @param tip         显示的文字
+ */
++ (void)showHudOnView:(UIView *)targetView tip:(NSString *)tip;
+
+/**
+ 移除hud
+ 
+ @param targetView  hud所在的view
+ */
++ (void)removeFromView:(UIView *)targetView;
+
+@end
